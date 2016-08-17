d57bbb21934edceec091113cc80d7f615af9272d
daytonpa <daytonpa@gmail.com>
2016-08-17 19:53:46 -0400

    Latest update 8/17/2016

--- recipes/pipeline_test.rb
+++ recipes/pipeline_test.rb
@@ -1,13 +1,24 @@
 # This recipe will grab an existing repo from GitHub and push it to a chef-server
 
 ## Create directory for the GitHub repos ##
+directory '/var/git_repos' do
+	user 		node['jenkins-centos']['user']
+	group		node['jenkins-centos']['group']
+	mode		'0644'
+end
+
+## Initialize git ##
+execute 'initialize_git' do
+	cwd '/var/git_repos'
+	command 'git init .'
+	action :run
+end
 
 ## Download the Flix repo from GitHub ##
-git 'deploy_flix' do
+git 'var/git_repos/deploy_flix' do
 	user 		node['jenkins-centos']['user']
 	group 		node['jenkins-centos']['group']
 
-	destination # folder to be determined at an appropriate time...
 	repository 	'git://github.com/daytonpa/deploy_flix.git'
 	reference 	'master'
 	action 		:sync
@@ -16,10 +27,16 @@ end
 
 ## Push cookbook to chef-server ##
 execute 'create_cookbook_path_directory' do
-	command		'cp /chef-repo/cookbooks/my_cookbooks/deploy_flix/ cookbooks/'
+	command		'cp /var/git_repos/deploy_flix/ cookbooks/'
 	action		:nothing
-	notifies 	:run, 'execute[add_cookbook_to_run_list]', :immediately
+	notifies 	:run, 'execute[upload_cookbook]', :immediately
 	not_if		'knife status | grep deploy_flix'
+end
+
+execute 'upload_cookbook' do
+	command 'knife cookbook upload deploy_flix'
+	action :nothing
+	notifies :run, 'execute[add_cookbook_to_run_list]', :immediately
 end
 
 execute 'add_cookbook_to_run_list' do
